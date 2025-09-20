dea814c9adca813e93d4bfd25a0ed9764220a227
league <league@imac-34.attlocal.net>
2025-09-20 16:30:19 -0700

    Up to test 5 in search sort algorithm, go back to more+more was skipped

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -1,5 +1,6 @@
 package _03_More_Algorithms;
 
+import java.util.Iterator;
 import java.util.List;
 
 /*
@@ -12,6 +13,46 @@ public class Algorithms {
     }
     
     public static int findBrokenEgg(List<String> eggs) {
-        return 0;
+      int i = -1;
+    	for (String item : eggs) {
+		i++;
+    	   if(item.equals("cracked")) {
+			return i;
+			
+		}
+	}
+    	return 0;
     }
+
+	public static Object countPearls(List<Boolean> oysters) {
+		int pearls = 0;
+		for (Boolean ya: oysters) {
+			if(ya == true) {
+				pearls++;
+			}
+		}
+		return pearls;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		double tallestPerson = 0;
+		for (double thing : peeps) {
+			if(thing>tallestPerson) {
+				tallestPerson = thing;
+			}
+		}
+		return tallestPerson;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+		int longestWordLength = 0;
+		String longestWord = null;
+		for(String jeff: words) {
+			if(jeff.length()>longestWordLength) {
+				longestWordLength = jeff.length();
+				longestWord = jeff;
+			}
+		}
+		return longestWord;
+	}
 }
